492cccb7679ee1375c6286fba349b2893e5a67a5
sourabhraikwar <sourabhraikwar11111@gmail.com>
2024-07-01 00:03:30 +0530

    complete user crud

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from bson import ObjectId
 from pydantic import BaseModel, EmailStr, Field
-
+from bson import ObjectId
+from typing import Optional
 
 class PyObjectId(ObjectId):
     @classmethod
@@ -8,32 +8,31 @@ class PyObjectId(ObjectId):
         yield cls.validate
 
     @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
+    def validate(cls, *args, **kwargs):
+        for v in args:
+            if not ObjectId.is_valid(v):
+                raise ValueError('Invalid objectid')
+            return ObjectId(v)
 
     @classmethod
     def __get_pydantic_json_schema__(cls, schema):
         schema.update(type="string")
         return schema
 
-
 class UserBase(BaseModel):
     username: str = Field(..., example="john_doe")
     email: EmailStr = Field(..., example="john.doe@example.com")
 
-
 class UserCreate(UserBase):
     password: str = Field(..., example="secret_password")
 
-
 class UserInDB(UserBase):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-
 
 class User(UserBase):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
 
     class Config:
-        json_encoders = {ObjectId: str}
+        json_encoders = {
+            ObjectId: str
+        }
